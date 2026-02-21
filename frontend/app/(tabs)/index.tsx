732f02abc2f564383650e5fbeea970ef3511e374
Fiona Yzabelle Gumaroy <126933200+fionayzabellegumaroy@users.noreply.github.com>
2026-02-21 15:33:25 -0500

    build: made frontend folder and server folder

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+import { HelloWave } from '@/frontend/components/hello-wave';
+import ParallaxScrollView from '@/frontend/components/parallax-scroll-view';
+import { ThemedText } from '@/frontend/components/themed-text';
+import { ThemedView } from '@/frontend/components/themed-view';
 import { Link } from 'expo-router';
 
 export default function HomeScreen() {
